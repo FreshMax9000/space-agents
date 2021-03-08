142539d2da81134ea29d65a0395e9b6e841aba8d
FreshMax9000 <maxmnaujokat@gmail.com>
2021-03-09 00:56:19 +0100

    Added some stupid stuff

--- main.py
+++ main.py
@@ -1,31 +1,79 @@
 import sys
-import copy
+import math
+import random
+import time
 #third party imports
 import pygame as pg
 from pygame.locals import *
 
 
-WIDTH = 900
+WIDTH = 1200
 HEIGHT = 900
-FPS = 30.0
+FPS = 30
 BORDER_EVASION_BASE = 1.1
+BOID_SPEED = 150
+BOIDS_COUNT = 100
+VISION = 50
+
+ALIGNMENT = 5
+COHESION = 0.5
+SEPERATION = 300
+
+fireSprites = pg.sprite.RenderUpdates()
+
+class DeadlyLaser(pg.sprite.Sprite):
+    image = pg.Surface((300, 5), pg.SRCALPHA)
+    pg.draw.rect(image, pg.Color('red'),
+        pg.rect.Rect(0, 0, 300, 300), width = 3)  
+
+    def __init__(self, position, rotation):
+        super().__init__()
+        self.position = position
+        self.rotation = rotation
+        self.image = pg.transform.rotate(self.__class__.image, -self.rotation.as_polar()[1])
+        self.rect = self.image.get_rect(center=self.position + self.rotation * 170)
+        fireSprites.add(self)
+
+    def update(self, boids):
+        pg.sprite.spritecollide(self, boids, dokill=True)
+        self.kill()
 
 
 class Boid(pg.sprite.Sprite):
-    image = pg.image.load("images/x_wing_test.png")
+    image = pg.Surface((10, 10), pg.SRCALPHA)
+    pg.draw.polygon(image, pg.Color('white'),
+        [(15, 5), (0, 2), (0, 8)])
     
     def __init__(self, position: pg.Vector2):
         super().__init__()
         self.position = position
-        self.velocity = 50
-        self.direction = pg.Vector2(10.0, 10.0)
+        self.velocity = BOID_SPEED
+        self.direction = pg.Vector2(10.0, 10.0).normalize()
         self.heading = 0.0
+        self.image = pg.transform.rotate(self.__class__.image, -self.heading)
+        self.rect = self.image.get_rect(center=self.position)
+
+    def fire(self, boids):
+        DeadlyLaser(self.position, self.direction)
+
+    def filter_boids(self, boids, radius):
+        boids2 = boids
+        for boid in boids:
+            distance = self.position.distance_to(boid.position)
+            if distance > radius or distance == 0.0:
+                boids2.remove(boid)
+        if len(boids) == 0:
+            print(len(boids2))
+        return boids2
 
     def update(self, boids, dt: float):
+        if random.randrange(100) == 99:
+            self.fire(boids)
+        boids = self.filter_boids(boids, VISION)
         self.direction = self.compute(boids)
         self.position += self.direction * self.velocity * dt 
         speed, self.heading = self.direction.as_polar()
-        self.image = pg.transform.rotate(Boid.image, -self.heading)
+        self.image = pg.transform.rotate(self.__class__.image, -self.heading)
         self.rect = self.image.get_rect(center=self.position)
 
     def alignment(self, boids):
@@ -33,7 +81,6 @@ class Boid(pg.sprite.Sprite):
         for boid in boids:
             if not self.position.distance_squared_to(boid.position) == 0.0:
                 average_heading_vector += boid.direction.normalize()
-        #TODO fix
         if average_heading_vector.length() == 0.0:
             return average_heading_vector
         return average_heading_vector.normalize()
@@ -41,11 +88,19 @@ class Boid(pg.sprite.Sprite):
 
     def cohesion(self, boids):
         swarm_center = pg.Vector2((0, 0))
+        counted = 0
         for boid in boids:
             if not self.position.distance_squared_to(boid.position) == 0.0:
-                swarm_center += boid.position
-        swarm_center = swarm_center / len(boids)
-        return (swarm_center - self.position)
+                if self.position.distance_to(boid.position) < 150:
+                    swarm_center += boid.position
+                    counted += 1
+        #swarm_center = swarm_center / len(boids)
+        if counted == 0:
+            return swarm_center
+        swarm_center = swarm_center / counted
+        if (swarm_center - self.position).length() == 0.0 or True:
+            return swarm_center - self.position
+        return (swarm_center - self.position).normalize()
 
 
     def separation(self, boids):
@@ -53,7 +108,9 @@ class Boid(pg.sprite.Sprite):
         for boid in boids:
             if not self.position.distance_squared_to(boid.position) == 0.0:
                 aversion_vector += -1 / self.position.distance_squared_to(boid.position) * (boid.position - self.position)
-        return aversion_vector
+        if aversion_vector.length() == 0.0 or True:
+            return aversion_vector
+        return aversion_vector.normalize()
 
     def border_aversion(self):
         aversion_vector = pg.Vector2((0, 0))
@@ -67,27 +124,43 @@ class Boid(pg.sprite.Sprite):
             aversion_vector.y = -(BORDER_EVASION_BASE ** (self.position.y - HEIGHT))
         return aversion_vector
 
-    def compute(self, boids):
-        direction_vector = self.alignment(boids)
-        direction_vector += self.cohesion(boids) * 0.01
-        direction_vector += self.separation(boids)
-        direction_vector += self.border_aversion()
-        direction_vector = direction_vector.normalize()
-        return direction_vector
+    def limit_turn(self, desired_turn: pg.Vector2):
+        return (self.direction + desired_turn * 0.5).normalize()
 
+    def compute(self, boids):
+        direction_vector = self.alignment(boids) * ALIGNMENT
+        direction_vector += self.cohesion(boids) * COHESION
+        direction_vector += self.separation(boids) * SEPERATION
+        direction_vector += self.border_aversion()
+        #direction_vector = direction_vector.normalize()
+        return self.limit_turn(direction_vector)
+
+class XWing(Boid):
+    image = pg.image.load("images/x_wing_test.png")
+
+class TieFighter(Boid):
+    pass
 
 def draw(screen, background, boids):
     boids.clear(screen, background)
+    fireSprites.clear(screen, background)
+    lasers = fireSprites.draw(screen)
     dirty = boids.draw(screen)
     pg.display.update(dirty)
+    pg.display.update(lasers)
+    #pg.display.flip()
 
 def update(boids, dt):
+    old_boids = boids.copy()
+    for laser in fireSprites:
+        laser.update(boids)
     for b in boids:
-        b.update(boids, dt)
+        b.update(old_boids.copy(), dt)
 
 def main():
     pg.init()
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+    screen.set_alpha(255)
 
     background = pg.Surface(screen.get_size())
     background.fill(pg.Color("black"))
@@ -95,9 +168,9 @@ def main():
     fps_clock = pg.time.Clock()
 
     boids = pg.sprite.RenderUpdates()
-    for i in range(10):
-        for j in range(20):
-            boids.add(Boid(pg.Vector2((i * 30, j * 30))))
+    for i in range(int(math.sqrt(BOIDS_COUNT))):
+        for j in range(int(math.sqrt(BOIDS_COUNT))):
+            boids.add(XWing(pg.Vector2((i * 30 + 200, j * 30 + 200))))
 
     while(True):
         for event in pg.event.get():
