1d91d1b8f04500a39cd2c3a62a6efc09dd83dbc3
FreshMax9000 <maxmnaujokat@gmail.com>
2021-03-29 10:18:53 +0200

    Made Vision Range bigger than laser range

--- lasers.py
+++ lasers.py
@@ -3,16 +3,16 @@ import pygame as pg
 import constants as const
 
 class DeadlyLaserRed(pg.sprite.Sprite):
-    image = pg.Surface((const.VISION, 5), pg.SRCALPHA)
+    image = pg.Surface((const.LASER_LENGTH, 5), pg.SRCALPHA)
     pg.draw.rect(image, pg.Color('red'),
-        pg.rect.Rect(0, 0, const.VISION, 5), width = const.VISION)  
+        pg.rect.Rect(0, 0, const.LASER_LENGTH, 5), width = const.LASER_LENGTH)  
 
     def __init__(self, position, rotation, laser_sprites):
         super().__init__()
         self.position = position
         self.rotation = rotation
         self.image = pg.transform.rotate(self.__class__.image, -self.rotation.as_polar()[1])
-        self.rect = self.image.get_rect(center=self.position + self.rotation * (const.VISION / 2.0 + 20))
+        self.rect = self.image.get_rect(center=self.position + self.rotation * (const.LASER_LENGTH / 2.0 + 20))
         laser_sprites.add(self)
 
     def update(self, boids):
@@ -20,6 +20,6 @@ class DeadlyLaserRed(pg.sprite.Sprite):
         self.kill()
 
 class DeadlyLaserGreen(DeadlyLaserRed):
-    image = pg.Surface((const.VISION, 5), pg.SRCALPHA)
+    image = pg.Surface((const.LASER_LENGTH, 5), pg.SRCALPHA)
     pg.draw.rect(image, pg.Color('green'),
-        pg.rect.Rect(0, 0, const.VISION, 5), width = const.VISION)
+        pg.rect.Rect(0, 0, const.LASER_LENGTH, 5), width = const.LASER_LENGTH)
